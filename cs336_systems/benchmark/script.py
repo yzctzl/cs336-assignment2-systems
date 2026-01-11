e7894ccf12b2b51b3fda0fab1cd2b7d8e7b23cc7
yzctzl <yzctzl@gmail.com>
2026-01-11 21:36:31 +0800

    benchmarking_script finish

--- cs336_systems/benchmark/script.py
+++ cs336_systems/benchmark/script.py
@@ -1,11 +1,10 @@
 import time
-from collections.abc import Iterator
-from typing import cast
 
 import numpy as np
+import pandas as pd
 import torch
 from cs336_basics.config import Configures, TrainConfig
-from cs336_basics.data import get_batch_iterator, load_checkpoint, save_checkpoint
+from cs336_basics.data import get_batch_iterator
 from cs336_basics.model import TransformerLM
 from cs336_basics.optimizer import (
     AdamW,
@@ -19,120 +18,83 @@ from jsonargparse import CLI
 from torch import nn, optim
 
 
-@torch.no_grad()
-def valid(model: nn.Module, valid_iter: Iterator, cfg: Configures, iters: int = 10, dtype: torch.dtype | None = None):
-    """
-    get 10 sample from valid set and calculate the mean loss
-    """
-    # change model work mode to valid/inference
-    model.eval()
-    losses = torch.zeros(iters)
-
-    for k in range(iters):
-        x, y = next(valid_iter)
-
-        with torch.autocast(device_type="cuda", dtype=dtype):
-            logits = model(x)
-            loss = cross_entropy(logits, y)
-        losses[k] = loss.detach()
-
-    # change back to train mode
-    model.train()
-    return losses.mean().item()
+MODEL_SIZES = {
+    "small": {"d_model": 768, "d_ff": 3072, "num_layers": 12, "num_heads": 12},
+    "medium": {"d_model": 1024, "d_ff": 4096, "num_layers": 24, "num_heads": 16},
+    "large": {"d_model": 1280, "d_ff": 5120, "num_layers": 36, "num_heads": 20},
+    "xl": {"d_model": 1600, "d_ff": 6400, "num_layers": 48, "num_heads": 25},
+    "2.7B": {"d_model": 2560, "d_ff": 10240, "num_layers": 32, "num_heads": 32},
+}
+result = []
 
 
-def train(
+def benchmark_train(
     model: nn.Module,
     optimizer: optim.Optimizer,
-    start_step: int,
     cfg: Configures,
-    scaler: torch.GradScaler,
     train_set: np.ndarray,
-    valid_set: np.ndarray,
     dtype: torch.dtype,
+    size_name: str,
 ):
-    # set the module in training mode
     model.train()
 
     tc: TrainConfig = cfg.train
     device = cfg.model.device
 
-    # data iterator
     train_iter = get_batch_iterator(train_set, tc.batch_size, cfg.model.context_length, device)
-    valid_iter = get_batch_iterator(valid_set, tc.batch_size, cfg.model.context_length, device)
 
-    t0 = time.perf_counter()
-    for it in range(start_step, tc.steps):
-        # x, y = get_batch(train_set, tc.batch_size, cfg.model.context_length, device)
-        optimizer.zero_grad(set_to_none=True)
+    try:
+        for it in range(tc.steps):
+            optimizer.zero_grad(set_to_none=True)
 
-        # use warm up + cosin lr dency schedule
-        if tc.lr_scheduler == "cosine":
-            lr = get_lr_cosine_schedule(it, tc.lr_max, tc.lr_min, tc.t_w, tc.t_c)
-        if tc.lr_scheduler == "wsd":
-            lr = get_lr_wsd_schedule(it, tc.lr_max, tc.lr_min, tc.steps, tc.t_w, tc.t_c)
-        # update lr in all optimizer params groups
-        for param_group in optimizer.param_groups:
-            param_group["lr"] = lr
+            if tc.lr_scheduler == "cosine":
+                lr = get_lr_cosine_schedule(it, tc.lr_max, tc.lr_min, tc.t_w, tc.t_c)
+            if tc.lr_scheduler == "wsd":
+                lr = get_lr_wsd_schedule(it, tc.lr_max, tc.lr_min, tc.steps, tc.t_w, tc.t_c)
+            for param_group in optimizer.param_groups:
+                param_group["lr"] = lr
 
-        # use gradient accumulation to simulate a larger batch size
-        accum_loss = torch.zeros(1, device=device)
-        for micro_step in range(tc.accum_steps):
             x, y = next(train_iter)
 
-            _t0 = time.perf_counter()
+            t0 = time.perf_counter()
             with torch.autocast(device_type="cuda", dtype=dtype):
                 logits = model(x)
-                loss = cross_entropy(logits, y) / tc.accum_steps
-            # Wait for all kernels in all streams on a CUDA device to complete, then count time.
+                loss = cross_entropy(logits, y)
             torch.cuda.synchronize()
-            _t1 = time.perf_counter()
-            accum_loss += loss.detach()
 
-            # Since PyTorch sums gradients in the .grad attribute by default, calling backward on each
-            # micro-batch loss (scaled by 1/accum_steps) computes the average gradient for the full batch size.
-            # scale the loss to prevent gradient underflow when using mixed precision (fp16).
-            scaler.scale(loss).backward()
-            # Wait for all kernels in all streams on a CUDA device to complete, then count time.
-            torch.cuda.synchronize()
-            _t2 = time.perf_counter()
-            print(f"Step {it:04d} | Forward Time: {_t1 - _t0:.6f} | Backward Time: {_t2 - _t1:.6f}")
-
-        # unscale before clipping to ensure the clipping threshold is applied to the actual gradient values.
-        scaler.unscale_(optimizer)
-        # clip the gradients to prevent them from exploding
-        total_norm = gradient_clipping(model.parameters(), tc.grad_clip)
-        # do optimizer step with scaler
-        scaler.step(optimizer)
-        scaler.update()
-
-        # log and save checkpoint after each interval steps
-        if (it + 1) % tc.interval == 0 or it == 0:
-            # Wait for all kernels in all streams on a CUDA device to complete, then count time.
-            torch.cuda.synchronize()
             t1 = time.perf_counter()
-            dt = t1 - t0
-            tps = (tc.batch_size * cfg.model.context_length * tc.accum_steps * tc.interval) / dt
+            loss.backward()
+            torch.cuda.synchronize()
 
-            # calc valid loss
-            vloss = valid(model, valid_iter, cfg, dtype=dtype)
-            # log to wandb
-            accum_loss_ = accum_loss.item()
-            print(
-                f"Step {it:04d} | Time: {dt:.6f} | Valid Loss: {vloss:.4f} | Norm: {total_norm:.4f} | "
-                f"Train Loss: {accum_loss_:.4f} | TPS: {tps:.1f} | LR: {lr:.2e}"
-            )
+            t2 = time.perf_counter()
+            gradient_clipping(model.parameters(), tc.grad_clip)
+            optimizer.step()
+            t3 = time.perf_counter()
 
-            if tc.checkpoint:
-                save_checkpoint(model, optimizer, it, f"./dist/checkpoint_{it:04d}_{vloss:.4f}.pt")
-            t0 = time.perf_counter()
+            if it > 0:
+                result.append({
+                    "Model": size_name,
+                    "Context": cfg.model.context_length,
+                    "Forward(ms)": (t1-t0) * 1000,
+                    "Backward(ms)": (t2-t1) * 1000,
+                    "Step(ms)": (t3-t2) * 1000,
+                    "Total(ms)": (t3-t0) * 1000,
+                })
+
+    except torch.cuda.OutOfMemoryError:
+        print(f"⚠️ {size_name}:{cfg.model.context_length} OOM!")
+        result.append({
+                    "Model": size_name,
+                    "Context": cfg.model.context_length,
+                    "Forward(ms)": np.nan,
+                    "Backward(ms)": np.nan,
+                    "Step(ms)": np.nan,
+                    "Total(ms)": np.nan
+                })
+        return False
 
 
-def main(
-    cfg: Configures,
-    fp16: bool = False,
-    resume: str | None = None,
-):
+def main(cfg: Configures):
     """
     Benchmarking script for TinyStories models.
 
@@ -141,41 +103,63 @@ def main(
         fp16: Enable fp16 + grad_scaler training.
         resume: Path to a checkpoint file to resume from.
     """
-    # seed
+
     torch.manual_seed(cfg.seed)
-    # precision
-    if fp16:
-        dtype = torch.float16
-    elif torch.cuda.get_device_capability() >= (8, 0):
+    if torch.cuda.get_device_capability() >= (8, 0):
         dtype = torch.bfloat16
         torch.set_float32_matmul_precision("high")
     else:
         dtype = torch.float32
 
-    # to scale gradients and prevent underflow during float16 mixed precision training
-    scaler = torch.GradScaler(cfg.model.device, enabled=(dtype == torch.float16))
-    # lazy load file
     train_set = np.load(cfg.data.train)
-    valid_set = np.load(cfg.data.valid)
-    # init Model
-    model = TransformerLM(**cfg.model.model_dump())
-    model.to(cfg.model.device)
-    # init Optimizer
-    if cfg.optimizer.type == "adamw":
-        optimizer = AdamW(model.parameters(), **cfg.optimizer.model_dump())
-    if cfg.optimizer.type == "muon":
-        optimizer = Muon(model, **cfg.optimizer.model_dump())
 
-    # load from checkpoint
-    start_step = 0
-    if resume:
-        start_step = load_checkpoint(resume, model, optimizer)
+    for size_name, params in MODEL_SIZES.items():
+        print(f"Benchmarking {size_name}...")
 
-    # compile the model to speedup
-    model = cast(nn.Module, torch.compile(model))
+        cfg.model.d_model = params["d_model"]
+        cfg.model.d_ff = params["d_ff"]
+        cfg.model.num_layers = params["num_layers"]
+        cfg.model.num_heads = params["num_heads"]
 
-    # train
-    train(model, optimizer, start_step, cfg, scaler, train_set, valid_set, dtype)
+        model = TransformerLM(**cfg.model.model_dump())
+        model.to(cfg.model.device)
+
+        if cfg.optimizer.type == "adamw":
+            optimizer = AdamW(model.parameters(), **cfg.optimizer.model_dump())
+        else:
+            optimizer = Muon(model, **cfg.optimizer.model_dump())
+
+        benchmark_train(model, optimizer, cfg, train_set, dtype, size_name)
+
+        del model, optimizer
+        torch.cuda.empty_cache()
+
+    model_order = list(MODEL_SIZES.keys())
+    df = pd.DataFrame(result)
+    df['Model'] = pd.Categorical(df['Model'], categories=model_order, ordered=True)
+    summary = df.groupby(["Model", "Context"], observed=True).agg({
+        "Forward(ms)": "mean",
+        "Backward(ms)": "mean",
+        "Step(ms)": "mean",
+        "Total(ms)": ["mean", "std"],
+    })
+    summary.columns = [
+        "Forward(ms)", 
+        "Backward(ms)", 
+        "Step(ms)", 
+        "Total(ms)", 
+        "Total_Std",
+    ]
+    summary = summary.reset_index()
+    summary["CV(%)"] = (summary["Total_Std"] / summary["Total(ms)"]) * 100
+
+    final_cols = [
+        "Model", "Context", "Forward(ms)", "Backward(ms)", 
+        "Step(ms)", "Total(ms)", "CV(%)"
+    ]
+
+    print("\n## Benchmark Summary (ms)")
+    print(summary[final_cols].to_markdown(index=False, floatfmt=".3f"))
 
 
 if __name__ == "__main__":
